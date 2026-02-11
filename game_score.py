a363feaa544669ef1cee47980d7023b7a7ee676c
DevStudent26011462 <student26011462@university.edu>
2026-02-11 22:09:10 +0530

    Languge Of AI Subjective Assignment

---
+++ game_score.py
@@ -0,0 +1,21 @@
+player_name = input("Enter the player's name: ")
+games_played = int(input("Enter the number of games played: "))
+total_score = int(input("Enter the total score: "))
+average_score = total_score / games_played
+print("\nPlayer:", player_name)
+print("\nGames Played:", games_played)
+print("\nTotal Score:", total_score)
+print("\nAverage Score:", average_score)
+
+
+Enter the player's name: Nag1,Nag2,Nag3,Nag4,Nag5,Nag6,Nag7,Nag8,Nag9,Nag10
+Enter the number of games played: 10
+Enter the total score: 115
+
+Player: Nag1,Nag2,Nag3,Nag4,Nag5,Nag6,Nag7,Nag8,Nag9,Nag10
+
+Games Played: 10
+
+Total Score: 115
+
+Average Score: 11.5
